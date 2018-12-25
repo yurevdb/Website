0f220c41cbd19d62bba84485820470c07ad55c3f
yurevdb <yurevdb@gmail.com>
2018-12-25 22:28:06 +0100

    Trying out React

---
+++ precache-manifest.69686a091c07bfb62de16136c00d052a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f18cb1634785f8253d76",
+    "url": "/static/js/main.f18cb163.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f18cb1634785f8253d76",
+    "url": "/static/css/main.65e24603.chunk.css"
+  },
+  {
+    "revision": "bc557c6d384c41e409b5c61c106bb5eb",
+    "url": "/index.html"
+  }
+];
