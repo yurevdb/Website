55aa381a637c60eaab6e31222c00af3644bfe1e2
Yuré Vanderbruggen <yurevdb@gmail.com>
2019-05-10 22:06:33 +0200

    New

---
+++ precache-manifest.dcb0ab610b707bb6999eb161808fef5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000cf524a090416e7c189650ab7fd370",
+    "url": "yurevdb.com/index.html"
+  },
+  {
+    "revision": "bb1227dbe4a194e4cdd1",
+    "url": "yurevdb.com/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "283c615f880046d60ebd",
+    "url": "yurevdb.com/static/js/2.a864196f.chunk.js"
+  },
+  {
+    "revision": "bb1227dbe4a194e4cdd1",
+    "url": "yurevdb.com/static/js/main.3731608e.chunk.js"
+  },
+  {
+    "revision": "164f27dce880b61655f9",
+    "url": "yurevdb.com/static/js/runtime~main.711d0c31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "yurevdb.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
